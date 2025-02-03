98cfe30c99963ba9a06d095830a5131ec453da3e
Muhammad Rizwan <muhammadrizwandnp266@gmail.com>
2025-02-03 19:18:13 +0500

    fixes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::group(['prefix'=>'category','as'=>'category.'],function(){
+        Route::get('/',[CategoryController::class,'index'])->name('index');
+    });
 });
 
 require __DIR__.'/auth.php';
